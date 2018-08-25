5b55c43782a78191d4f433bc0817c037c355dd4f
William Bork <williambork33@gmail.com>
2018-08-25 16:10:21 -0500

    Updating Readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 - A multiplayer version of the classic game of Rock, Paper, Scissors.
 
+- Live link: https://williambork33.github.io/RPSMultiplayer/
+
 - Built with HTML, CSS, Bootstrap, JavaScript, and jQuery. Uses Firebase for Realtime Database.
 
 - https://github.com/WilliamBork33
--- assets/javascript.js
+++ assets/javascript.js
@@ -18,23 +18,22 @@ $(document).ready(function () {
 
 
     //Declaring variables.
-    var userName = "USER";
-    var computerName = "COMPUTER";
-    var userChoiceword = "";
-    var computerChoicenumber = "";
-    var computerChoiceword = "";
-    var userScore = 0;
-    var computerScore = 0;
+    var playerName1 = "PLAYER1";
+    var playerName2 = "PLAYER2";
+    var playerChoiceword1 = "";
+    var playerChoiceword2 = "";
+    var playerScore1 = 0;
+    var playerScore2 = 0;
     var tieScore = 0;
     var audioElement;
     var audioPreference;
 
 
 
-    //Get both the user and computer's name and push it to the HTML.
-    var userName = "User" + prompt("Your name?");
-    var computerName = "Computer" + prompt("Computer name?");
-
+    //Get both the player1's and player2's name and push it to the HTML.
+    var playerName1 = "Player 1: " + prompt("Player 1 name?");
+    var playerName2 = "Player 2: " + prompt("Player 2 name?");
+    
 
 
     //Firebase is always watching for changes to the data.
@@ -45,12 +44,12 @@ $(document).ready(function () {
         console.log(snapshot.val());
 
         //Log the value of the various properties
-        console.log("userName: " + snapshot.val().userName);
-        console.log("computerName: " + snapshot.val().computerName);
-        console.log("userChoiceword: " + snapshot.val().userChoiceword);
-        console.log("computerChoiceword: " + snapshot.val().computerChoiceword);
-        console.log("userScore: " + snapshot.val().userScore);
-        console.log("computerScore: " + snapshot.val().computerScore);
+        console.log("playerName1: " + snapshot.val().playerName1);
+        console.log("playerName2: " + snapshot.val().playerName2);
+        console.log("playerChoiceword1: " + snapshot.val().playerChoiceword1);
+        console.log("playerChoiceword2: " + snapshot.val().playerChoiceword2);
+        console.log("playerScore1: " + snapshot.val().playerScore1);
+        console.log("playererScore2: " + snapshot.val().playerScore2);
         console.log("tieScore: " + snapshot.val().tieScore);
 
       // If any errors are experienced, log them to console.
@@ -60,168 +59,326 @@ $(document).ready(function () {
 
 
 
-    //Prints the name that the user used for themselves and the computer.
-    document.getElementById("human_name").innerHTML = userName;
-    document.getElementById("computer_name").innerHTML = computerName;
+    //Prints the names that the players used for themselves.
+    document.getElementById("human_name").innerHTML = playerName1;
+    document.getElementById("computer_name").innerHTML = playerName2;
 
-    //Function for computer to select random options.
-    function computerSelection() {
-        computerChoicenumber = Math.floor(Math.random() * 3) + 1;
-        computerChoiceword = "";
 
-        if (computerChoicenumber == 1)
-            {
-                computerChoiceword = "rock";
-            }
-        else if (computerChoicenumber == 2)
-            {
-                computerChoiceword = "paper";
-            }
-        else if (computerChoicenumber == 3)
-            {
-                computerChoiceword = "scissors";
-            }
+
+
+    var turn1finished = false;
+    var turn2finished = false;
+
+    if (playerChoiceword1 == "")
+    {
+        playerTurn1();
+        console.log("1")
+    }
+    if (playerChoiceword1 == "rock")
+    {
+        playerTurn2();
+        console.log("2")
+    }
+    else
+    {
+        console.log("3");
+    }
+
+
+    function playerTurn1() {
+        //OnClick event responds to player1's click/touch on buttons and saves the choice in memory.
+        $(".btn-rock").on("click", function () {
+            playerChoiceword1 = "rock";
+            player1Rock();
+            
+            
+
+
+            //Referencing the Firebase database.
+            database.ref().set({
+                playerName1: playerName1,
+                playerName2: playerName2,
+                playerChoiceword1: playerChoiceword1,
+                playerChoiceword2: playerChoiceword2,
+                playerScore1: playerScore2,
+                playerScore2: playerScore2,
+                tieScore: tieScore,
+            });
+        });
+
+
+        //OnClick event responds to player1's click/touch on buttons and saves the choice in memory.
+        $(".btn-paper").on("click", function () {
+            playerChoiceword1 = "paper";
+            player1Paper();
+
+            //Referencing the Firebase database.
+            database.ref().set({
+                playerName1: playerName1,
+                playerName2: playerName2,
+                playerChoiceword1: playerChoiceword1,
+                playerChoiceword2: playerChoiceword2,
+                playerScore1: playerScore2,
+                playerScore2: playerScore2,
+                tieScore: tieScore,
+            });
+        });
+
+
+        
+        //OnClick event responds to player1's click/touch on buttons and saves the choice in memory.
+        $(".btn-scissors").on("click", function () {
+            playerChoiceword1 = "scissors";
+            player1Scissors();
+
+            //Referencing the Firebase database.
+            database.ref().set({
+                playerName1: playerName1,
+                playerName2: playerName2,
+                playerChoiceword1: playerChoiceword1,
+                playerChoiceword2: playerChoiceword2,
+                playerScore1: playerScore2,
+                playerScore2: playerScore2,
+                tieScore: tieScore,
+            });
+        });
     }
 
 
 
-    //OnClick event responds to user click/touch on buttons then runs game logic.
-    $(".btn-rock").on("click", function () {
-        computerSelection()
-        userRock();
 
-        //Referencing the Firebase database.
-        database.ref().set({
-            userName: userName,
-            computerName: computerName,
-            userChoiceword: userChoiceword,
-            computerChoiceword: computerChoiceword,
-            userScore: userScore,
-            computerScore: computerScore,
-            tieScore: tieScore,
+    function playerTurn2() {
+
+        //OnClick event responds to player2's click/touch on buttons and saves the choice in memory.
+        $(".btn-rock").on("click", function () {
+            playerChoiceword2 = "rock";
+            player2Rock();
+          
+            //Referencing the Firebase database.
+            database.ref().set({
+                playerName1: playerName1,
+                playerName2: playerName2,
+                playerChoiceword1: playerChoiceword1,
+                playerChoiceword2: playerChoiceword2,
+                playerScore1: playerScore2,
+                playerScore2: playerScore2,
+                tieScore: tieScore,
+            });
         });
-    });
 
 
+        //OnClick event responds to player2's click/touch on buttons and saves the choice in memory.
+        $(".btn-paper").on("click", function () {
+            playerChoiceword2 = "paper";
+            player2Paper();
 
-    //OnClick event responds to user click/touch on buttons then runs game logic.
-    $(".btn-paper").on("click", function () {
-        computerSelection()
-        userPaper();
-
-        //Referencing the Firebase database.
-        database.ref().set({
-            userName: userName,
-            computerName: computerName,
-            computerChoiceword: computerChoiceword,
-            userScore: userScore,
-            computerScore: computerScore,
-            tieScore: tieScore,
+            //Referencing the Firebase database.
+            database.ref().set({
+                playerName1: playerName1,
+                playerName2: playerName2,
+                playerChoiceword1: playerChoiceword1,
+                playerChoiceword2: playerChoiceword2,
+                playerScore1: playerScore2,
+                playerScore2: playerScore2,
+                tieScore: tieScore,
+            });
         });
-    });
 
 
     
-    //OnClick event responds to user click/touch on buttons then runs game logic.
-    $(".btn-scissors").on("click", function () {
-        computerSelection()
-        userScissors();
+        //OnClick event responds to player2's click/touch on buttons and saves the choice in memory.
+        $(".btn-scissors").on("click", function () {
+            playerChoiceword2 = "scissors";
+            player2Scissors();
 
-        //Referencing the Firebase database.
-        database.ref().set({
-            userName: userName,
-            computerName: computerName,
-            userChoiceword: userChoiceword,
-            computerChoiceword: computerChoiceword,
-            userScore: userScore,
-            computerScore: computerScore,
-            tieScore: tieScore,
+            //Referencing the Firebase database.
+            database.ref().set({
+                playerName1: playerName1,
+                playerName2: playerName2,
+                playerChoiceword1: playerChoiceword1,
+                playerChoiceword2: playerChoiceword2,
+                playerScore1: playerScore2,
+                playerScore2: playerScore2,
+                tieScore: tieScore,
+            });
         });
-    });
+   
+    }
 
+    //Function for when player1 selects "ROCK".
+    function player1Rock() {
 
+        playerChoiceword1 = "rock";
 
-    //Function for when user selects "ROCK".
-    function userRock() {
-        userChoiceword = "rock";
-        if (computerChoiceword == "rock")
+        if (playerChoiceword1 == "rock" & playerChoiceword2 == "scissors")
         {
-            //Function for a user tie result.
+            //Function for a player1 win result.
+            player1Wins();
+            resultWin();
+        }
+        else if (playerChoiceword1 == "rock" & playerChoiceword2 == "paper")
+        {
+            //Function for a player2 win result.
+            player2Wins();
+            resultLose();
+        }
+        else if (playerChoiceword1 == "rock" & playerChoiceword2 == "rock")
+        {
+            //Function for a player1 tie result.
             tie();
             resultTied();
         }
-        else if (computerChoiceword == "scissors")
-        {
-            //Function for a user win result.
-            userWins();
-            resultWin();
-        }
-        else if (computerChoiceword == "paper")
-        {
-            //Function for a user lose result.
-            computerWins();
-            resultLose();
-        }
     }
 
-    //Function for when user selects "SCISSORS".
-    function userScissors() {
-        userChoiceword = "scissors";
-        if (computerChoiceword == "scissors")
+    //Function for when player1 selects "PAPER".
+    function player1Paper() {
+
+        playerChoiceword1 = "paper";
+
+        if (playerChoiceword1 == "paper" & playerChoiceword2 == "rock")
         {
-            //Function for a user tie result.
+            //Function for a player1 win result.
+            player1Wins();
+            resultWin();
+        }
+        else if (playerChoiceword1 == "paper" & playerChoiceword2 == "scissors")
+        {
+            //Function for a player2 win result.
+            player2Wins();
+            resultLose();
+        }
+        else if (playerChoiceword1 == "paper" & playerChoiceword2 == "paper")
+        {
+            //Function for a player1 tie result.
             tie();
             resultTied();
         }
-        else if (computerChoiceword == "paper")
-        {
-            //Function for a user win result.
-            userWins();
-            resultWin();
-        }
-        else if (computerChoiceword == "rock")
-        {
-            //Function for a user lose result.
-            computerWins();
-            resultLose();
-        }
     }
 
-    //Function for when user selects "PAPER".
-    function userPaper() {
-        userChoiceword = "paper";
-        if (computerChoiceword == "paper")
+    //Function for when player1 selects "SCISSORS".
+    function player1Scissors() {
+
+        playerChoiceword1 = "scissors";
+
+        if (playerChoiceword1 == "scissors" & playerChoiceword2 == "paper")
         {
-            //Function for a user tie result.
+            //Function for a player1 win result.
+            player1Wins();
+            resultWin();
+        }
+        else if (playerChoiceword1 == "scissors" & playerChoiceword2 == "rock")
+        {
+            //Function for a player2 win result.
+            player2Wins();
+            resultLose();
+        }
+        else if (playerChoiceword1 == "scissors" & playerChoiceword2 == "scissors")
+        {
+            //Function for a player1 tie result.
             tie();
             resultTied();
         }
-        else if (computerChoiceword == "rock")
+    }
+  
+
+
+
+
+
+
+    //Function for when player2 selects "ROCK".
+    function player2Rock() {
+
+        playerChoiceword2 = "rock";
+
+        if (playerChoiceword2 == "rock" & playerChoiceword1 == "scissors")
         {
-            //Function for a user win result.
-            userWins();
+            //Function for a player2 win result.
+            player2Wins();
             resultWin();
         }
-        else if (computerChoiceword == "scissors")
+        else if (playerChoiceword2 == "rock" & playerChoiceword1 == "paper")
         {
-            //Function for a user lose result.
-            computerWins();
+            //Function for a player2 lose result.
+            player1Wins();
             resultLose();
         }
+        else if (playerChoiceword2 == "rock" & playerChoiceword1 == "rock")
+        {
+            //Function for a player2 tie result.
+            tie();
+            resultTied();
+        }
     }
+
+    //Function for when player2 selects "PAPER".
+    function player2Paper() {
+
+        playerChoiceword2 = "paper";
+
+        if (playerChoiceword2 == "paper" & playerChoiceword1 == "rock")
+        {
+            //Function for a player2 win result.
+            player2Wins();
+            resultWin();
+        }
+        else if (playerChoiceword2 == "paper" & playerChoiceword1 == "scissors")
+        {
+            //Function for a player2 lose result.
+            player1Wins();
+            resultLose();
+        }
+        else if (playerChoiceword2 == "paper" & playerChoiceword1 == "paper")
+        {
+            //Function for a player2 tie result.
+            tie();
+            resultTied();
+        }
+    }
+
+//Function for when player2 selects "SCISSORS".
+function player2Scissors() {
+
+    playerChoiceword2 = "scissors";
+
+    if (playerChoiceword2 == "scissors" & playerChoiceword1 == "paper")
+    {
+        //Function for a player2 win result.
+        player2Wins();
+        resultWin();
+    }
+    else if (playerChoiceword2 == "scissors" & playerChoiceword1 == "rock")
+    {
+        //Function for a player2 lose result.
+        player1Wins();
+        resultLose();
+    }
+    else if (playerChoiceword2 == "scissors" & playerChoiceword1 == "scissors")
+    {
+        //Function for a player2 tie result.
+        tie();
+        resultTied();
+    }
+}
+
+
+
+
 
     //Functions for win, lose, tie alert.
     function tie() {
-        alert("You choose: " + userChoiceword + "\nComputer choose: " + computerChoiceword + "\nYou TIED!")
+        alert("Player 1 choose: " + playerChoiceword1 + "\nPlayer 2 choose: " + playerChoiceword2 + "\nYou TIED!")
     }
 
-    function userWins() {
-        alert("You choose: " + userChoiceword + "\nComputer choose: " + computerChoiceword + "\nYou WIN!")
+    function player1Wins() {
+        alert("Player 1 choose: " + playerChoiceword1 + "\nPlayer 2 choose: " + playerChoiceword2 + "\nPlayer 1 WIN!")
     }
 
-    function computerWins() {
-        alert("You chose: " + userChoiceword + "\nComputer choose: " + computerChoiceword + "\nYou LOSE!")
+    function player2Wins() {
+        alert("Player 2 chose: " + playerChoiceword1 + "\nPlayer 2 choose: " + playerChoiceword2 + "\nPlayer 2 WINS!")
     }
+
+
 
     //Functions for keeping score.
     function resultTied() {
@@ -230,13 +387,13 @@ $(document).ready(function () {
     }
 
     function resultWin() {
-        userScore++;
-        document.getElementById("human_score").innerHTML = userScore;
+        playerScore1++;
+        document.getElementById("human_score").innerHTML = playerScore1;
     }
 
     function resultLose() {
-        computerScore++;
-        document.getElementById("computer_score").innerHTML = computerScore;
+        playerScore2++;
+        document.getElementById("computer_score").innerHTML = playerScore2;
     }
 
 });
